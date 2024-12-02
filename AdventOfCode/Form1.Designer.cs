087885767dc8503a19e1ef75193284263a06b5bd
Michał Białończyk <wujko95@gmail.com>
2024-12-02 19:11:38 +0100

    Add project files.

---
+++ AdventOfCode/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AdventOfCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnGetFile, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbPath, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnRun, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tbResult, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.37838F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.62162F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 144F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(744, 294);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGetFile.Location = new System.Drawing.Point(3, 3);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(738, 91);
+            this.btnGetFile.TabIndex = 0;
+            this.btnGetFile.Text = "Get File";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.btnGetFile_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPath.Location = new System.Drawing.Point(3, 100);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(738, 20);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRun.Location = new System.Drawing.Point(3, 126);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(738, 138);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "RURA";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbResult.Location = new System.Drawing.Point(3, 270);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(738, 20);
+            this.tbResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 294);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnGetFile;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ AdventOfCode/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AdventOfCode
+{
+    public partial class Form1 : Form
+    {
+        private string _path;
+        private IAlgoritm _algoritm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Application.StartupPath;
+                openFileDialog.Filter = "Txt (*.txt)|*.txt";
+                openFileDialog.FilterIndex = 1;
+                if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    _path = openFileDialog.FileName;
+                    tbPath.Text = _path;
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
