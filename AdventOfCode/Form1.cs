2df2d9d175e8cf8870a2c7c455c8c97fa0e41f87
Michał Białończyk <wujko95@gmail.com>
2024-12-03 17:58:28 +0100

    D3

--- AdventOfCode/Form1.cs
+++ AdventOfCode/Form1.cs
@@ -39,7 +39,7 @@ namespace AdventOfCode
         {
             try
             {
-                IAlgoritm algoritm = new D2();
+                IAlgoritm algoritm = new D3();
                 if (File.Exists(_path))
                 {
                     Settings.Default.Path = _path;
