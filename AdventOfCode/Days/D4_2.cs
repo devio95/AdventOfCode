9725bdcb5417e63826c2375f7113385f0b224c3d
Michał Białończyk <wujko95@gmail.com>
2024-12-08 16:46:28 +0100

    D4_2

---
+++ AdventOfCode/Days/D4_2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace AdventOfCode.Days
+{
+    public class D4_2 : IAlgoritm
+    {
+        private char[,] _chars;
+        private int _column;
+        private int _row;
+        private int _columns => _chars.GetLength(0);
+        private int _rows => _chars.GetLength(1);
+
+        public string Run(string text)
+        {
+            InsertData(text);
+            int result = 0;
+            for (int row = 1; row < _rows - 1; row++)
+            {
+                for (int column = 1; column < _columns - 1; column++)
+                {
+                    _column = column;
+                    _row = row;
+                    result += Check();
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private void InsertData(string text)
+        {
+            string[] lines = text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            _chars = new char[lines[0].Length, lines.Length];
+            int row = 0;
+            int column = 0;
+            foreach (string line in lines)
+            {
+                column = 0;
+                foreach (char c in line)
+                {
+                    _chars[column, row] = c;
+                    column++;
+                }
+                row++;
+            }
+        }
+
+        private int Check()
+        {
+            if (_chars[_column, _row] != 'A')
+            {
+                return 0;
+            }
+
+            int result = One() + Two() + Three() + Four();
+
+            return result >= 2 ? 1 : 0;
+        }
+
+        private int One()
+        {
+            bool result = CheckText(_chars[_column - 1, _row - 1], _chars[_column + 1, _row + 1]);
+            return result ? 1 : 0;
+        }
+
+        private int Two()
+        {
+            bool result = CheckText(_chars[_column + 1, _row - 1], _chars[_column - 1, _row + 1]);
+            return result ? 1 : 0;
+        }
+
+        private int Three()
+        {
+            bool result = CheckText(_chars[_column + 1, _row + 1], _chars[_column - 1, _row - 1]);
+            return result ? 1 : 0;
+        }
+
+        private int  Four()
+        {
+            bool result = CheckText(_chars[_column - 1, _row + 1], _chars[_column + 1, _row - 1]);
+            return result ? 1 : 0;
+        }
+
+        private bool CheckText(char a, char b)
+        {
+            char first = _chars[_column, _row];
+            return first == 'A' && a == 'M' && b == 'S';
+        }
+    }
+}
--- AdventOfCode/Form1.cs
+++ AdventOfCode/Form1.cs
@@ -39,7 +39,7 @@ namespace AdventOfCode
         {
             try
             {
-                IAlgoritm algoritm = new D3_2();
+                IAlgoritm algoritm = new D4_2();
                 if (File.Exists(_path))
                 {
                     Settings.Default.Path = _path;
