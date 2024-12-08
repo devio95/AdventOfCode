4e81f38a30231fc8a6266802926f545e0857e324
Michał Białończyk <wujko95@gmail.com>
2024-12-08 13:25:40 +0100

    D1_2

---
+++ AdventOfCode/Days/D1_2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class D1_2 : IAlgoritm
+    {
+        private List<int> _left;
+        private List<int> _right;
+        public string Run(string text)
+        {
+            string[] lines = text.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            _left = new List<int>();
+            _right = new List<int>();
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                _left.Add(int.Parse(columns[0]));
+                _right.Add(int.Parse(columns[1]));
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < _left.Count; i++)
+            {
+                sum += _left[i] * HowManyTimesInRightColumns(_left[i]);
+            }
+            return sum.ToString();
+        }
+
+        public int HowManyTimesInRightColumns(int number)
+        {
+            return _right.FindAll(x => x == number).Count();
+        }
+    }
+}
